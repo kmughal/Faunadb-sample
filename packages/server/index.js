ba5b60f598f54befa0dba06e6ae8815023824d8f
Khurram <KhurramShahzad@tfl.gov.uk>
2020-11-12 12:32:15 +0000

    add end point to fetch all todos by username

--- packages/server/index.js
+++ packages/server/index.js
@@ -7,11 +7,23 @@ require("dotenv").config();
 const app = Express();
 const client = new faunaDb.Client({ secret: process.env.FAUNDA_DB_SCRETE });
 
-const { Get, Match, Index, Create, Select, Collection } = faunaDb.query;
+const {
+  Get,
+  Match,
+  Index,
+  Create,
+  Select,
+  Collection,
+  Call,
+  Function: fn,
+  Paginate
+} = faunaDb.query;
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -40,16 +52,31 @@ app.get("/getReference", async (req, res) => {
 
 app.post("/add", async (req, res) => {
   const { email, text } = req.body;
-  
+
   if (!email || !text) {
     return res.send(400);
   }
   const data = {
-    user: Select("ref", Get(Match(Index("userbyemail"), email))),
+    user: Call(fn("getuserbyemail"), email),
     todo: [text],
   };
   await client.query(Create(Collection("todos"), { data }));
   res.send("new document created for " + email).status(200);
+});
+
+app.get("/alltodos", async (req, res) => {
+  const { email } = req.query;
+  if (!email) return res.send(404);
+
+  const result = await client.query(
+    Paginate(
+      Match(
+        Index("todosbyuser"),
+        Select("ref", Get(Match(Index("userbyemail"), email)))
+      )
+    )
+  );
+  res.send(result);
 });
 
 app.listen(3000, () => console.log("server started:"));
